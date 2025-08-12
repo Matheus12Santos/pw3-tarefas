9fce387da6513b45d8c7a8358e2902c200d1f989
unknown <matheus.vieira137@etec.sp.gov.br>
2025-08-12 10:59:42 -0300

    Aplicando o DTO

--- src/main/java/br/com/etechas/tarefas/controller/TarefaController.java
+++ src/main/java/br/com/etechas/tarefas/controller/TarefaController.java
@@ -1,5 +1,6 @@
 package br.com.etechas.tarefas.controller;
 
+import br.com.etechas.tarefas.dto.TarefaResponseDTO;
 import br.com.etechas.tarefas.entity.Tarefa;
 import br.com.etechas.tarefas.service.TarefaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +19,7 @@ public class TarefaController {
     private TarefaService service;
 
     @GetMapping
-    public List<Tarefa> listar(){
+    public List<TarefaResponseDTO> listar(){
         return service.findAll();
     }
 }
